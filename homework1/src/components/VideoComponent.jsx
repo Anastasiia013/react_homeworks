8e1a6a23500253b4df15ef40c1ddf7e1b1e5089b
ANASTASIIA <anastasiaposokhova@gmail.com>
2025-02-28 10:05:14 +0100

    ch hw1

--- homework1/src/components/VideoComponent.jsx
+++ homework1/src/components/VideoComponent.jsx
@@ -1,7 +1,7 @@
 const VideoComponent = () => {
     return (
         <div>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/tA1s65o_kYM?si=mT7dY7K2nxQDjqDs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/tA1s65o_kYM?si=mT7dY7K2nxQDjqDs" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>
             </iframe>
         </div>
     );
